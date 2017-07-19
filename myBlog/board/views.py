3bd235b0c395794ab7ceb821d48afe6754d98843
luoxuan <luoxuanro@gmail.com>
2017-07-19 18:11:44 +0800

    更新个人博客留言板

--- myBlog/board/views.py
+++ myBlog/board/views.py
@@ -4,12 +4,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Message
+import time
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
-    message_list = Message.objects.order_by('-pub_date')
-    template = loader.get_template('board/index.html')
-    context = {
-        'message_list': message_list,
-    }
-    return HttpResponse(template.render(context, request))
+    if request.method == 'POST':
+        # if 'submit' in request.POST:
+        user_message = request.POST['user_message']
+        user_name = request.POST['user_name']
+        if user_message.strip() != '':
+            if user_name == '':
+                user_name = 'NO_ONE'
+            now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+            Message.objects.create(message_text=user_message, message_name=user_name, support=0, pub_date=now)
+        return HttpResponseRedirect('/board/')
+        '''
+        else:
+        msg = request.GET['message']
+        count = msg.support
+        msg.support = count + 1
+        '''
+    else:
+        message_list = Message.objects.order_by('-pub_date')
+        template = loader.get_template('board/index.html')
+        context = {
+            'message_list': message_list,
+        }
+        return HttpResponse(template.render(context, request))
