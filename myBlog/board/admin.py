17fc2592d4bbe9e2c4c8086b917a77eff810d68c
luoxuan <luoxuanro@gmail.com>
2017-08-02 16:57:38 +0800

    修改admin

--- myBlog/board/admin.py
+++ myBlog/board/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-
 # Register your models here.
-from django.contrib import admin
-
 from .models import Message
 
-admin.site.register(Message)
+
+class MessageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Message',               {'fields': ['message_text']}),
+        ('Author', {'fields': ['message_name']}),
+        ('Date information', {'fields': ['pub_date']}),
+        ('Support', {'fields': ['support']}),
+    ]
+    list_display = ('message_text','message_name', 'pub_date')
+admin.site.register(Message, MessageAdmin)
